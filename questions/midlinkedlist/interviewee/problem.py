e054abcb9cf5d7f13867f67e1872eaed681e0bb1
Peter Gottesman <peter@petergottesman.com>
2018-01-31 15:00:36 -0500

    Added midlinkedlist problem.py for interviewee

--- questions/midlinkedlist/interviewee/problem.py
+++ questions/midlinkedlist/interviewee/problem.py
@@ -1,7 +1,22 @@
 #/usr/bin/python3
 
 # Question here
+# Implement a function to find the center element in a linked list
 #
-#
-#
-#
+# Input: Linked list node (head)
+# Output: Linked list node (center)
+
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+
+if __name__ == "__main__":
+    head = Node(1)
+    curNode = head
+    for idx in range(2,40): # values range from 1 to 39, result will be 20
+        curNode.next = Node(idx)
+        curNode = curNode.next
